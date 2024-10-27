4d0e470570d1498d7543c3fb76f23eccc822fd6d
vinayrajaikar <vinayrajaikar@gmail.com>
2024-10-27 22:25:54 +0530

    added news model

--- server/src/models/news.model.js
+++ server/src/models/news.model.js
@@ -1,3 +1,38 @@
-import mongoose, {Schema} from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import mongoose, { Schema } from "mongoose";
+
+const newsSchema = new mongoose.Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            refPath: 'userType'  // This dynamically references either "Farmer" or "Supplier" based on `userType`
+        },
+        userType: {
+            type: String,
+            required: true,
+            enum: ["Farmer", "Supplier"]  // Restricting to either "Farmer" or "Supplier"
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        link: {
+            type: String,
+            trim: true
+        },
+        upvotes: {
+            type: Number,
+            default: 0
+        },
+        downvotes: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const News = mongoose.model("News", newsSchema);
