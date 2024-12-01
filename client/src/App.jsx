a2b3881bc4b37afb06ad7b97a642d5f62d421903
YashwantHabib <yashwanthabib567@gmail.com>
2024-12-01 12:48:29 +0530

    Connection establishment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,7 +61,7 @@ const App = () => {
       <h1>{JSON.stringify(data)}</h1>
       <h1>{JSON.stringify(details)}</h1>
       <h3>{JSON.stringify(farmerDetails)}</h3>
-
+      <h1>Hackerrrr</h1>
     </div>
   );
 };
